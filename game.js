a5751aa234a3d3ee54b090dfab8779dd1b52fabd
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-12-06 15:45:22 +0530

    edit style of the gomoku board

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 function getGridHtml (m, n) {
-  let gridHtml = '<table>'
+  let gridHtml = '<table align="center">'
   for (let i = 0; i < m; ++i) {
     gridHtml += '<tr>'
     for (let j = 0; j < n; ++j) {
-      gridHtml += '<td style="width: 1.1em" id="' + (i * n + j) + '">' + '.' + '</td>'
+      gridHtml += '<td id="' + (i * n + j) + '">' + '<div class="empty"><div/>' + '</td>'
     }
     gridHtml += '</tr>'
   }
@@ -11,3 +11,10 @@ function getGridHtml (m, n) {
 }
 
 document.getElementById('grid').innerHTML = getGridHtml(19, 19)
+
+Module.addOnPostRun(() => {
+  const board = new Module.Board()
+  const search = new Module.Search(board)
+  console.log(search.calcBestMove(4, 0))
+  console.log(board.place(0, 0))
+})
