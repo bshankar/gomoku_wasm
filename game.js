b55a8c938a3e327788ae3aded045532d065834f8
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-12-07 11:23:37 +0530

    add some helper functions

--- game.js
+++ game.js
@@ -8,15 +8,37 @@ function getGridHtml (m, n) {
     gridHtml += '</tr>'
   }
   return gridHtml + '</table>' +
-    '<h3 class="eval" id="evaluation" align="center">Game started</h3>'
+    '<h3 class="eval" id="evalHeader" align="center">Game started</h3>'
 }
 
 document.getElementById('grid').innerHTML = getGridHtml(19, 19)
 
 Module.addOnPostRun(() => {
-  const board = new Module.Board()
-  const search = new Module.Search(board)
 
-  const mark = (player, cell) => { document.getElementById(cell).className = 'p' + player }
+  const $ = document.getElementById
+
+  function clearGame () {
+    const board = new Module.Board()
+    const search = new Module.Search(board)
+    for (let i = 0; i < 361; ++i) $(i).className = 'empty'
+  }
+  
+  function placeBead (player, cell) {
+    board.place(player, cell)
+    $(cell).className = 'p' + player
+  }
+
+  function displayAnalysis (bestMoveInfo) {
+    const analysis = 'Eval: ' + bestMoveInfo.eval + ' ' + bestMoveInfo.nodes + ' nodes visited'
+    $(bestMoveInfo.bestMove)[0].innerHTML = analysis
+  }
+
+  function makeBestMove (player, depth = 6) {
+    const bestMoveInfo = search.calcBestMove(depth, player)
+    displayAnalysis(bestMoveInfo)
+    placeBead(player, bestMoveInfo.bestMove)
+  }
+
+  let playerTurn = $('playWhiteCheckbox').checked
   
 })
