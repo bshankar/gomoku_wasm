cd41149e3f4ff202466c442ed82e7126d61e7460
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-12-09 16:41:20 +0530

    add new css classes to highlight current move

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ Module.addOnPostRun(() => {
     for (let i = 0; i < m; ++i) {
       gridHtml += '<tr>'
       for (let j = 0; j < n; ++j) {
-        gridHtml += '<td class="empty" id="' + (i * n + j) + '"></td>'
+        gridHtml += '<td class="box empty" id="' + (i * n + j) + '"></td>'
       }
       gridHtml += '</tr>'
     }
@@ -24,13 +24,13 @@ Module.addOnPostRun(() => {
   function startGame () {
     search = new Module.Search()
     playerTurn = $('playWhiteCheckbox').checked
-    for (let i = 0; i < 361; ++i) $('' + i).className = 'empty'
+    for (let i = 0; i < 361; ++i) $('' + i).className = 'box empty'
     if (playerTurn === false) makeBestMove()
   }
 
   function placeBead (cell) {
     search.place(currentPlayer, cell)
-    $(cell).className = 'p' + currentPlayer
+    $(cell).className = 'player' + currentPlayer
     togglePlayer()
   }
 
